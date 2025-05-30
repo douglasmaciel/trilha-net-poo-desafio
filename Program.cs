921c75f413d173f36e7546b0061a3137c0e025fa
Douglas S. Maciel <contato@douglasmaciel.com>
2025-05-30 06:22:35 -0300

    testa classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia phone1 = new Nokia("1", "Tijolão", "0987654565432123", 4);
+Iphone phone2 = new Iphone("2", "iPhone 6", "4876209876542113", 8);
+
+phone1.InstalarAplicativo("Opera Browser");
+phone2.InstalarAplicativo("WhatsApp");
