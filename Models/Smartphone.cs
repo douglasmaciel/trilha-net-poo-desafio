e43452b2ad20eae5407f47b003e621343ee207b9
Douglas S. Maciel <contato@douglasmaciel.com>
2025-05-30 05:51:26 -0300

    cria propriedades restantes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,6 +4,9 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
         public Smartphone(string numero)
         {
